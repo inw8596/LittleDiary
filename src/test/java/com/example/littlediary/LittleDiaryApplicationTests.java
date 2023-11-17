d0b66a5c0950a83ca3a5c98b8b5371cabc2183a9
inw8596 <inw8596@gmail.com>
2023-11-17 20:43:34 +0900

    Initial commit

---
+++ src/test/java/com/example/littlediary/LittleDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.littlediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
